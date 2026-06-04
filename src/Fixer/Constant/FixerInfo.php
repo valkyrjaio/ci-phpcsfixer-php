5bfd306c2142bb486b60771db72deabc1d713446
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-06-04 16:46:55 +0000

    [Release] Update FixerInfo for v26.1.5 release

--- src/Fixer/Constant/FixerInfo.php
+++ src/Fixer/Constant/FixerInfo.php
@@ -20,12 +20,12 @@ final class FixerInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.1.4';
+    public const string VERSION = '26.1.5';
 
     /**
      * The Fixer package version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'May 30 2026 08:47:18 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'June 4 2026 09:46:11 MST';
 }
