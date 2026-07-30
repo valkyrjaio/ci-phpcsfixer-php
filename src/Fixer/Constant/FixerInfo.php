990bcfdbef17da984b7e2e20e377efe7b48141a7
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-07-30 05:43:42 +0000

    [v26.2.0] chore: Update FixerInfo for the release

--- src/Fixer/Constant/FixerInfo.php
+++ src/Fixer/Constant/FixerInfo.php
@@ -20,12 +20,12 @@ final class FixerInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.1.14';
+    public const string VERSION = '26.2.0';
 
     /**
      * The Fixer package version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'July 25 2026 14:25:13 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'July 29 2026 22:43:01 MST';
 }
